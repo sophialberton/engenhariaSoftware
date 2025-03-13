6a632a5114cc82b81fa921673fcff99bff3af6a8
Sophia <sophia.alberton@gmail.com>
2025-03-12 21:49:00 -0300

    Update

--- Recursividade/AlocacaoDinamica.c
+++ Recursividade/AlocacaoDinamica.c
@@ -6,7 +6,7 @@ int main()
     /*malloc(sizeof(int)): Aloca dinamicamente um espaço na memória grande o suficiente para armazenar um inteiro (int). O sizeof(int) retorna o tamanho de um int em bytes (geralmente 4 bytes).
 (int *): Faz a conversão do tipo void* retornado pelo malloc para int*, indicando que o ponteiro armazenará um inteiro.
 pont: É um ponteiro para int que armazenará o endereço da memória recém-alocada.
-Importante: O malloc pode falhar e retornar NULL. Sempre é bom verificar:*/
+Importante: O malloc pode falhgitar e retornar NULL. Sempre é bom verificar:*/
     int *pont = (int *)malloc(sizeof(int)); //alocação dinamica de memoria
 
     *pont = 42;
