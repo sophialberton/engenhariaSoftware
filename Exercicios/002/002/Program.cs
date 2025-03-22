09d182a08f56608139f0ed06b7f9c05a0787ab6c
Sophia <sophia.alberton@gmail.com>
2025-03-22 19:42:10 -0300

    Exercicio 2 executado com sucesso

---
+++ Exercicios/002/002/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sophia Picasky Alberton");
+Produto produto = new Produto("Garrafa", 2, 16.25);
+
+Console.Write("Valor total que esta disponivel no estoque:");
+produto.CalcularTotal();
+
+Console.ReadKey();
