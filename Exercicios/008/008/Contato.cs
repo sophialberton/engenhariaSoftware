9d8951555ef24549b59fcf7fc3729c2ec6147f8e
Sophia <sophia.alberton@gmail.com>
2025-03-23 15:22:47 -0300

    Exercicio 8 executado com sucesso

---
+++ Exercicios/008/008/Contato.cs
@@ -0,0 +1,14 @@
+public class Contato
+{
+
+    public string nome;
+    public long numeroDeTelefone;
+    public string email;
+
+    public Contato(string nom, long num, string email)
+    {
+        this.nome= nom;
+        this.numeroDeTelefone = num;
+        this.email = email;
+    }
+}
