47b2379debfcfe5c574d3eae5ab27726d21a246d
Sophia <sophia.alberton@gmail.com>
2025-03-26 19:54:16 -0300

    Booble sort

--- BoobleSort/ex1.cpp
+++ BoobleSort/ex1.cpp
@@ -10,20 +10,28 @@ main()
     int x, y, troca, compara;
     string temp;
 
-    dados.push_back("Catatu");
-    dados.push_back("Xunda");
-    dados.push_back("Penelope");
-    dados.push_back("Ze buscape");
-    dados.push_back("Tiao Gaviao");
-    dados.push_back("Mutley");
-    dados.push_back("Babalu");
-    dados.push_back("Pepe Legal");
+    dados.push_back("Catatu"); //0
+    dados.push_back("Xunda"); //1
+    dados.push_back("Penelope"); //2
+    dados.push_back("Ze buscape"); //3
+    dados.push_back("Tiao Gaviao"); //4
+    dados.push_back("Mutley"); //5
+    dados.push_back("Babalu"); //6
+    dados.push_back("Pepe Legal"); //7
 
-//Aplica o algoritimo Bubble Sort v1
-    for (x=0; x<dados.size(); x++)//dados.size sinaliza a quantidade de de dados.
+    // Aplica o algoritimo Bubble Sort v1
+    for (x = 0; x < dados.size(); x++) // dados.size sinaliza a quantidade de de dados.
     {
-
+        for (y = x + 1; y < dados.size(); y++)
+        {
+            if(dados[y]<dados[x]){
+                temp=dados[x];
+                dados[x] = dados[y];
+                dados[y] = temp;
+            }
+        }
     }
+    //final booble sort.
 
     return 0;
 }
