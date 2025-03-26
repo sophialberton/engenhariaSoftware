37ae636faf22e4229bfad00589d32f4b270a9b00
Sophia <sophia.alberton@gmail.com>
2025-03-26 19:50:22 -0300

    Conteudo novo

---
+++ BoobleSort/ex1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+main()
+{
+    vector<string> dados;
+    int x, y, troca, compara;
+    string temp;
+
+    dados.push_back("Catatu");
+    dados.push_back("Xunda");
+    dados.push_back("Penelope");
+    dados.push_back("Ze buscape");
+    dados.push_back("Tiao Gaviao");
+    dados.push_back("Mutley");
+    dados.push_back("Babalu");
+    dados.push_back("Pepe Legal");
+
+//Aplica o algoritimo Bubble Sort v1
+    for (x=0; x<dados.size(); x++)//dados.size sinaliza a quantidade de de dados.
+    {
+
+    }
+
+    return 0;
+}
