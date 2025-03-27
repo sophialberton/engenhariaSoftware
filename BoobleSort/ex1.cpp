b187a16d59b94a4b49d874f947d458a8542811f0
Sophia <sophia.alberton@gmail.com>
2025-03-26 21:10:52 -0300

    Booble sort v1 e v1.1

--- BoobleSort/ex1.cpp
+++ BoobleSort/ex1.cpp
@@ -10,28 +10,65 @@ main()
     int x, y, troca, compara;
     string temp;
 
-    dados.push_back("Catatu"); //0
-    dados.push_back("Xunda"); //1
-    dados.push_back("Penelope"); //2
-    dados.push_back("Ze buscape"); //3
-    dados.push_back("Tiao Gaviao"); //4
-    dados.push_back("Mutley"); //5
-    dados.push_back("Babalu"); //6
-    dados.push_back("Pepe Legal"); //7
+    dados.push_back("Catatu");      // 0
+    dados.push_back("Xunda");       // 1
+    dados.push_back("Penelope");    // 2
+    dados.push_back("Ze buscape");  // 3
+    dados.push_back("Tiao Gaviao"); // 4
+    dados.push_back("Mutley");      // 5
+    dados.push_back("Babalu");      // 6
+    dados.push_back("Pepe Legal");  // 7
 
-    // Aplica o algoritimo Bubble Sort v1
+    // mostra os dados sem odernação
+    cout << "Sem dados ordenados" << endl;
+    for (x = 0; x<dados.size(); x++)
+    {
+        cout << dados[x] << endl;
+    }
+
+    /*Aplica o algoritimo Bubble Sort v1
     for (x = 0; x < dados.size(); x++) // dados.size sinaliza a quantidade de de dados.
     {
         for (y = x + 1; y < dados.size(); y++)
         {
-            if(dados[y]<dados[x]){
-                temp=dados[x];
+            if (dados[y] < dados[x])
+            {
+                temp = dados[x];
                 dados[x] = dados[y];
                 dados[y] = temp;
             }
         }
     }
-    //final booble sort.
+     final booble sort. */
 
-    return 0;
+    // Aplica o algoritimo Bubble Sort v1.1
+    compara=0;
+    troca=0;
+    for (x = 0; x < dados.size(); x++) // dados.size sinaliza a quantidade de de dados.
+    {
+        for (y = x + 1; y < dados.size(); y++)
+        {
+            compara++;
+            if (dados[y] < dados[x])
+            {
+                troca++;
+                temp = dados[x];
+                dados[x] = dados[y];
+                dados[y] = temp;
+            }
+        }
+    }
+    // final booble sort.
+
+    // mostra os dados ordenados
+    cout << endl;
+
+    cout << "DADOS ORDENADOS"<< endl;
+    for (x = 0; x < dados.size(); x++)
+    {
+        cout << dados[x] << endl;
+    }
+    cout << "qtd de omparacoes : " << compara << endl;
+
+    cout << "Qtd de trocas : " << troca << endl;
 }
