5ba011630609bd64a638c77974355c2d92351dbd
Sophia <sophia.alberton@gmail.com>
2025-04-25 22:15:01 -0300

    Inicio sistema de alugar festas

---
+++ Sistema-Festas/Festas/Festas/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Tela tela = new Tela(80, 25, ConsoleColor.Green, ConsoleColor.Yellow);
+//int opcao;
+
+while (true)
+{
+    tela.prepararTela();
+    /*
+    tela.desenharMoldura(0, 0, 79, 24);
+    tela.desenharMoldura(1, 1, 15, 10);
+    tela.desenharMoldura(17, 5, 70, 20);
+    */
+    Console.ReadKey();
+
+    /*
+    prepaar o menu com opções de cruddo cliente 
+    usuario esoclhe uma opcao 
+    executar a opcao escolhda na casse cliente
+    */
+}
