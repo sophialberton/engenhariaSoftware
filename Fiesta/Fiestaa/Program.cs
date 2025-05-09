c578595e22bd8122dd99f3781f07f2fcb0d31aa2
Sophia <sophia.alberton@gmail.com>
2025-05-09 19:42:20 -0300

    fiesta!!

---
+++ Fiesta/Fiestaa/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+*   Sistema de aluguel para festas
+*   Fiesta!!!
+*/
+
+List<string> opcoes = new List<string>(); //criou lista opcoes e uma lista nova
+opcoes.Add("1 - Manter Clientes"); //adicionou a opção 1
+opcoes.Add("2 - Manter Temas"); //adicionou a opção 2
+opcoes.Add("3 - Registrar Aluguel"); //adicionou a opção 3
+opcoes.Add("4 - Registrar Devolução"); //adicionou a opção 4
+opcoes.Add("0 - Sair"); //adicionou a opção 0
+
+Tela tela = new Tela(80, 25, ConsoleColor.Black, ConsoleColor.Magenta); //com base na classe Tela, criou uma nova tela com 80 colunas/25 linhas/cor de fundo preto/cor de texto roxo
+string opcao;
+
+while (true)
+{
+    tela.prepararTela("Fiesta!!!");
+    opcao = tela.mostrarMenu(opcoes, 1, 2);
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Manter Clientes");
+            break;
+        case "2":
+            Console.WriteLine("Manter Temas");
+            break;
+        case "3":
+            Console.WriteLine("Registrar Aluguel");
+            break;
+        case "4":
+            Console.WriteLine("Registrar Devolução");
+            break;
+        case "0":
+            Console.WriteLine("Saindo...");
+            return;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+    Console.WriteLine("Pressione qualquer tecla para continuar...");
+    Console.ReadKey();
+}
---
+++ Fiesta/Fiestaa/Tela.cs
@@ -0,0 +1,100 @@
+public class Tela
+{
+    private int largura;
+    private int altura;
+    private ConsoleColor corFundo;
+    private ConsoleColor corTexto;
+
+
+    public Tela(int largura, int altura, ConsoleColor fundo, ConsoleColor texto)
+    {
+        this.largura = largura;
+        this.altura = altura;
+        this.corFundo = fundo;
+        this.corTexto = texto;
+    }
+
+
+    public void prepararTela(string titulo = "")
+    {
+        Console.ForegroundColor = corTexto;
+        Console.BackgroundColor = corFundo;
+        Console.Clear();
+        this.desenharMoldura(0, 0, this.largura, this.altura);
+        this.desenharMoldura(0, 0, this.largura, 2);
+        this.centralizar(titulo, 1, 0, this.largura);
+    }
+
+
+    public void desenharMoldura(int colIni, int linIni, int colFin, int linFin)
+    {
+        // linhas horizontais
+        for (int x = colIni; x <= colFin; x++)
+        {
+            Console.SetCursorPosition(x, linIni);
+            Console.Write("-");
+            Console.SetCursorPosition(x, linFin);
+            Console.Write("-");
+        }
+        // linhas verticais
+        for (int y = linIni; y <= linFin; y++)
+        {
+            Console.SetCursorPosition(colIni, y);
+            Console.Write("|");
+            Console.SetCursorPosition(colFin, y);
+            Console.Write("|");
+        }
+        // cantos
+        Console.SetCursorPosition(colIni, linIni); Console.Write("+");
+        Console.SetCursorPosition(colIni, linFin); Console.Write("+");
+        Console.SetCursorPosition(colFin, linIni); Console.Write("+");
+        Console.SetCursorPosition(colFin, linFin); Console.Write("+");
+    }
+
+
+    public void centralizar(string texto, int lin, int colIni, int colFin)
+    {
+        int colTexto = ((colFin - colIni - texto.Length) / 2) + colIni;
+        Console.SetCursorPosition(colTexto, lin);
+        Console.Write(texto);
+    }
+
+
+    public string mostrarMenu(List<string> opcoes, int colIni, int linIni)
+    {
+        string opcaoEscolhida = "";
+
+        // procura pela maior largura entre as opções do menu
+        int largura = 0;
+        foreach (string opcao in opcoes)
+        {
+            if (opcao.Length > largura)
+            {
+                largura = opcao.Length;
+            }
+        }
+
+        // define a largura e altura para a moldura do menu
+        int colFin = colIni + largura + 1;
+        int linFin = linIni + opcoes.Count + 2;
+        this.desenharMoldura(colIni, linIni, colFin, linFin);
+
+        // mostra as opções do menu
+        colIni++;
+        linIni++;
+        for (int i = 0; i < opcoes.Count; i++)
+        {
+            Console.SetCursorPosition(colIni, linIni);
+            Console.WriteLine(opcoes[i]);
+            linIni++;
+        }
+
+        // pergunta a opção escolhida
+        Console.SetCursorPosition(colIni, linIni);
+        Console.Write("Escolha uma opção: ");
+        opcaoEscolhida = Console.ReadLine();
+
+        // retorna a opção escolhida
+        return opcaoEscolhida;
+    }
+}
