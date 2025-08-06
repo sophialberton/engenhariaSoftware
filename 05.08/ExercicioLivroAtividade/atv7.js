248f72cd18e3a754a3445079e4545571a426e84e
sophialberton <sophia.alberton@gmail.com>
2025-08-06 12:46:58 -0300

    atividade 7

---
+++ 05.08/ExercicioLivroAtividade/atv7.js
@@ -0,0 +1,8 @@
+let pessoa = {
+ nome: "sophia",
+ idade: 19,
+ cidade: "Joinville",
+};
+console.log("Nome: " + pessoa.nome);
+console.log("Idade: " + pessoa.idade);
+console.log("Cidade: " + pessoa.cidade);
