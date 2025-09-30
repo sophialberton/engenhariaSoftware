6d30a88a51cea3bb8af7d31b3997ae640c1442d3
sophialberton <sophia.alberton@gmail.com>
2025-09-30 20:45:00 -0300

    testes

--- TesteSoftware/30.09/ChaiExercicio/ChaiExercicio/test/caixa.test.js
+++ TesteSoftware/30.09/ChaiExercicio/ChaiExercicio/test/caixa.test.js
@@ -7,11 +7,25 @@ const logger = require('../src/logger');
 
 describe('Caixa - Testes Unitários', () => {
   it('deve calcular o total corretamente', () => {
-    // TODO
+    const venda = [
+      { produto: 'pao', preco: 0.5, quantidade: 10 },    // Total: 5.0
+      { produto: 'leite', preco: 4.0, quantidade: 2 },   // Total: 8.0
+      { produto: 'cafe', preco: 15.0, quantidade: 1 }    // Total: 15.0
+    ];
+    // O total esperado é 5.0 + 8.0 + 15.0 = 28.0
+    const total = caixa.calcularTotal(venda);
+    expect(total).to.equal(28.0);
   });
 
   it('deve lançar erro para venda inválida', () => {
-    // TODO
+    const vendaVazia = () => caixa.registrarVenda([]);
+    expect(vendaVazia).to.throw('Venda inválida');
+
+    const vendaNaoArray = () => caixa.registrarVenda('nao-e-array');
+    expect(vendaNaoArray).to.throw('Venda inválida');
+
+    const vendaNula = () => caixa.registrarVenda(null);
+    expect(vendaNula).to.throw('Venda inválida');
   });
 });
 
@@ -25,14 +39,34 @@ describe('Caixa - Testes com Stub', () => {
   afterEach(() => sinon.restore());
 
   it('deve chamar removerDoEstoque para cada item da venda', () => {
-    // TODO
+    const venda = [
+      { produto: 'pao', quantidade: 2 },
+      { produto: 'leite', quantidade: 1 }
+    ];
+    caixa.registrarVenda(venda);
+
+    expect(estoque.removerDoEstoque.callCount).to.equal(2);
+    expect(estoque.removerDoEstoque.getCall(0).calledWith('pao', 2)).to.be.true;
+    expect(estoque.removerDoEstoque.getCall(1).calledWith('leite', 1)).to.be.true;
   });
 
   it('deve chamar logger.log após venda', () => {
-    // TODO
+    const venda = [{ produto: 'cafe', preco: 15.0, quantidade: 1 }];
+    caixa.registrarVenda(venda);
+
+    // Verificamos se logger.log foi chamado uma vez
+    expect(logger.log.calledOnce).to.be.true;
+    // Verificamos a mensagem exata do log
+    expect(logger.log.calledWith('Venda registrada. Total: R$ 15.00')).to.be.true;
   });
 
   it('deve chamar logger.error para venda inválida', () => {
-    // TODO
+    try {
+      caixa.registrarVenda([]);
+    } catch (e) {
+      // O erro é esperado, o teste continua
+    }
+    expect(logger.error.calledOnce).to.be.true;
+    expect(logger.error.calledWith('Venda inválida')).to.be.true;
   });
 });
--- TesteSoftware/30.09/ChaiExercicio/ChaiExercicio/test/estoque.test.js
+++ TesteSoftware/30.09/ChaiExercicio/ChaiExercicio/test/estoque.test.js
@@ -5,38 +5,68 @@ const estoque = require('../src/estoque');
 const logger = require('../src/logger');
 
 describe('Estoque - Testes Unitários', () => {
-  afterEach(() => sinon.restore());
+
+  let estadoOriginalEstoque;
+
+  beforeEach(() => {
+    estadoOriginalEstoque = JSON.parse(JSON.stringify(estoque._estoqueInterno));
+  });
+
+  afterEach(() => {
+    Object.keys(estoque._estoqueInterno).forEach(key => delete estoque._estoqueInterno[key]);
+    Object.assign(estoque._estoqueInterno, estadoOriginalEstoque);
+    sinon.restore();
+  });
+
 
   it('deve retornar quantidade correta em consultarEstoque', () => {
-    // TODO
+    expect(estoque.consultarEstoque('pao')).to.equal(100);
+    expect(estoque.consultarEstoque('manteiga')).to.equal(0);
   });
 
   it('deve adicionar itens ao estoque', () => {
-    // TODO
+    estoque.adicionarAoEstoque('leite', 10);
+    expect(estoque.consultarEstoque('leite')).to.equal(60);
   });
 
   it('deve lançar erro ao adicionar quantidade inválida', () => {
-    // TODO
+    expect(() => estoque.adicionarAoEstoque('cafe', 0)).to.throw('Quantidade deve ser maior que zero');
+    expect(() => estoque.adicionarAoEstoque('cafe', -5)).to.throw('Quantidade deve ser maior que zero');
   });
 
   it('deve remover itens do estoque', () => {
-    // TODO
+    estoque.removerDoEstoque('pao', 20);
+    expect(estoque.consultarEstoque('pao')).to.equal(80);
   });
 
   it('deve lançar erro ao remover mais do que o disponível', () => {
-    // TODO
+    expect(() => estoque.removerDoEstoque('cafe', 31)).to.throw('Estoque insuficiente');
   });
 });
 
 describe('Estoque - Testes com Stub (Logger)', () => {
-  beforeEach(() => sinon.stub(logger, 'log'));
-  afterEach(() => sinon.restore());
+  beforeEach(() => {
+    sinon.stub(logger, 'log');
+    sinon.stub(logger, 'error');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
 
   it('deve chamar logger.log ao adicionar item ao estoque', () => {
-    // TODO
+    estoque.adicionarAoEstoque('pao', 10);
+    expect(logger.log.calledOnce).to.be.true;
+    expect(logger.log.calledWith('Adicionado 10 unidades de pao ao estoque')).to.be.true;
   });
 
   it('deve chamar logger.error ao tentar adicionar quantidade inválida', () => {
-    // TODO
+    try {
+      estoque.adicionarAoEstoque('leite', 0);
+    } catch (e) {
+//erro esperado
+    }
+    expect(logger.error.calledOnce).to.be.true;
+    expect(logger.error.calledWith('Quantidade inválida: 0')).to.be.true;
   });
 });
